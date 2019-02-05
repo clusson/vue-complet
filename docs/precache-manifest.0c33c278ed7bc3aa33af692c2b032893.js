6854852c8ceefde16b4fe94f84330f9963f2572b
Clément Lusson <clementlusson@gmail.com>
2019-02-05 11:55:10 +0100

    Update

---
+++ docs/precache-manifest.0c33c278ed7bc3aa33af692c2b032893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0401dedae1a6405f61c",
+    "url": "/js/about.d59feb68.js"
+  },
+  {
+    "revision": "5a555515dea921cec061",
+    "url": "/css/app.e8ea34f2.css"
+  },
+  {
+    "revision": "5a555515dea921cec061",
+    "url": "/js/app.b4a3ad4a.js"
+  },
+  {
+    "revision": "88d3726ae9c2ecda4b0e",
+    "url": "/js/chunk-vendors.657d1ea9.js"
+  },
+  {
+    "revision": "7917a3e3fec4ee2e3dbf645e3259633d",
+    "url": "/img/logo.7917a3e3.png"
+  },
+  {
+    "revision": "466fd632d05a239711650438544b4de3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
