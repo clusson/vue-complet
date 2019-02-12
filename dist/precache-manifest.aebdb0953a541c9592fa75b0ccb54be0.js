012c01395f7f521a98d8bd3df6f3a7c63d25768f
Clément Lusson <clementlusson@gmail.com>
2019-02-12 14:52:46 +0100

    delete dist from gitignore

---
+++ dist/precache-manifest.aebdb0953a541c9592fa75b0ccb54be0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "db9346d02e206e5e644f",
+    "url": "/js/about.3a0b45af.js"
+  },
+  {
+    "revision": "97291bfd4dba10ec09a4",
+    "url": "/css/app.0e433876.css"
+  },
+  {
+    "revision": "97291bfd4dba10ec09a4",
+    "url": "/js/app.5cdd304d.js"
+  },
+  {
+    "revision": "4ddafe60490a18871d4e",
+    "url": "/css/chunk-vendors.f11d0a9b.css"
+  },
+  {
+    "revision": "4ddafe60490a18871d4e",
+    "url": "/js/chunk-vendors.3ed12e1e.js"
+  },
+  {
+    "revision": "b6a9bc4538ce311af8104d903bf7d30a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
