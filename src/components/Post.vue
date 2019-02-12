2152b9d852c0a7ffdc8d68ed7041cc783bdbe468
Clément Lusson <clementlusson@gmail.com>
2019-02-12 15:33:58 +0100

    position to read more

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,29 +1,31 @@
 <template>
-	<v-layout>
-		<v-flex xs12>
-			<v-card>
-				<v-card-title primary-title>
-					<div>
-						<div class="headline">
-							<span v-html="post.title.rendered"></span>
-						</div>
-						<span class="grey--text">
-							Posté le
-							<span v-html="$options.filters.formatDate(post.date, 100)"></span>
-						</span>
-					</div>
-				</v-card-title>
-				<v-card-text>
-					<span v-html="$options.filters.truncate(post.content.rendered, 100)">
-					</span>
-					<span class="grey--text">{{ sinceDate }}</span>
-				</v-card-text>
-				<v-card-actions>
-					<v-btn flat color="orange" :to="path">Lire la suite</v-btn>
-				</v-card-actions>
-			</v-card>
-		</v-flex>
-	</v-layout>
+  <v-layout>
+    <v-flex xs12>
+      <v-card>
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">
+              <span v-html="post.title.rendered"></span>
+            </div>
+            <span class="grey--text">
+              Posté le
+              <span v-html="$options.filters.formatDate(post.date, 100)"></span>
+            </span>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          <span v-html="$options.filters.truncate(post.content.rendered, 100)">
+          </span>
+          <span class="grey--text">{{ sinceDate }}</span>
+        </v-card-text>
+        <v-card-actions>
+          <div class="read">
+            <v-btn flat color="orange" :to="path">Lire la suite</v-btn>
+          </div>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -65,4 +67,10 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.read {
+  position: absolute;
+  right: 10px;
+  bottom: 10px;
+}
+</style>
