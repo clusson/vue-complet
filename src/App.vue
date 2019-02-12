a66c5cdb6cceae0b338e4d787b2def4f2bca560e
Clément Lusson <clementlusson@gmail.com>
2019-02-12 15:28:19 +0100

    add style

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,38 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span class="font-weight-light">WORDPRESS API</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn flat :to="{ name: 'blog' }">
-        <span class="mr-2">Blog</span>
-      </v-btn>
-    </v-toolbar>
+	<v-app>
+		<v-toolbar app>
+			<v-toolbar-title class="headline text-uppercase">
+				<span>VUEJS cours</span>
+				<v-spacer></v-spacer>
+				<span class="font-weight-light">WORDPRESS API</span>
+			</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<span>
+				<v-btn flat :to="{ name: 'blog' }">
+					<span class="mr-2">Blog</span>
+				</v-btn>
+			</span>
+			<span>
+				<v-btn flat href="https://github.com/clusson/vue-complet" target="_blank">
+					<span class="mr-2">Github Repo</span>
+				</v-btn>
+			</span>
+		</v-toolbar>
 
-    <v-content>
-      <router-view></router-view>
-    </v-content>
-  </v-app>
+		<v-content>
+			<router-view></router-view>
+		</v-content>
+		<Footer />
+	</v-app>
 </template>
 
 <script>
+import Footer from "./components/Footer";
 export default {
   name: "App",
-
+  components: {
+    Footer
+  },
   data() {
     return {
       //
---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer class="pa-3">
+    <v-spacer></v-spacer>
+    <div>powered by Clément LUSSON</div>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {};
+  }
+};
+</script>
